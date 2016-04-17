ee00482556664ea1d2a9298a6d3c7eb2d7ecd00d
Dan Meakin <dan@danmeakin.com>
2016-04-17 14:07:26 +0200

    Refactor code in BinaryChopD; add Javadocs

--- src/main/java/BinaryChopD.java
+++ src/main/java/BinaryChopD.java
@@ -8,44 +8,62 @@ public class BinaryChopD implements BinaryChop {
     private int[] intArray;
     private int target;
 
+    /**
+     * Carries out the binary chop.
+     *
+     * @param target the target value
+     * @param intArray the array to search/chop
+     * @return the index of target within intArray, or -1 if not present
+     */
     public int chop(int target, int[] intArray) {
         this.intArray = intArray;
         this.target = target;
-        List<Integer> ep = getEndPoint();
-        if (ep.size() == 0 || ep.get(0) >= intArray.length) {
-            return -1;
-        } else if (target == intArray[ep.get(0)]){
-            return ep.get(0);
-        } else {
-            return -1;
-        }
+        return getEndPoint();
+
     }
 
-    private List<Integer> getEndPoint() {
+    /**
+     * Finds the final index for the target within intArray.
+     *
+     * This method returns an integer representing the index of target within
+     * the intArray. If target is not found, this method returns -1.
+     *
+     * @return index of target within intArray, or -1 if not found
+     */
+    private int getEndPoint() {
         List<Integer> indices = new ArrayList<>();
         Iterator<List<Integer>> iterator = listIterator();
+
         while (iterator.hasNext()) {
             indices = iterator.next();
         }
-        return indices;
+        boolean notFound = indices.size() == 0
+                || indices.get(0) >= intArray.length
+                || target != intArray[indices.get(0)];
+        if (notFound) {
+            return -1;
+        } else {
+            return indices.get(0);
+        }
     }
 
+    /**
+     * Creates an iterator which operates over a list of indices to values in
+     * intArray, iteratively carrying-out a binary search for the index of the
+     * target value.
+     *
+     * @return iterator which iterates through the list of indices towards the
+     *         target value
+     */
     private Iterator<List<Integer>> listIterator() {
-        List<Integer> l = new ArrayList<>();
-        for (int i = 0; i < intArray.length; i++) {
-            l.add(i);
-        }
-        System.out.println(l.toString());
+        List<Integer> l = intArrayToList();
+
         return new Iterator<List<Integer>>() {
             private List<Integer> currSubList = l;
-            private boolean hasNextEntry = true;
+            private boolean hasNextEntry = l.size() > 0;
 
             @Override
             public List<Integer> next() {
-                if (currSubList.size() == 0) {
-                    hasNextEntry = false;
-                    return currSubList;
-                }
                 // Get the middle index of the current sub-list
                 int midPoint = currSubList.size() / 2;
                 // Get the index of the intArray represented by the value at the
@@ -56,17 +74,16 @@ public class BinaryChopD implements BinaryChop {
 
                 if (target == midValue) {
                     currSubList = Arrays.asList(new Integer[]{midIndex});
-                    hasNextEntry = false;
                 } else if (target < midValue) {
                     currSubList = currSubList.subList(0, midPoint);
                 } else if (target > midValue) {
                     if (currSubList.size() <= midPoint + 1) {
-                        hasNextEntry = false;
                         currSubList = new ArrayList<>();
                     } else {
                         currSubList = currSubList.subList(midPoint + 1, currSubList.size());
                     }
-                } else {
+                }
+                if (currSubList.size() <= 1) {
                     hasNextEntry = false;
                 }
                 return currSubList;
@@ -77,5 +94,18 @@ public class BinaryChopD implements BinaryChop {
                 return hasNextEntry;
             }
         };
+    }
+
+    /**
+     * Converts the intArray to a list of Integers.
+     *
+     * @return intArray as list of Integers
+     */
+    private List<Integer> intArrayToList() {
+        List<Integer> l = new ArrayList<>();
+        for (int i = 0; i < intArray.length; i++) {
+            l.add(i);
+        }
+        return l;
     }
 }
